4035f5bd424be4cc50ccd040068e51bb4d9bd15f
Gerardo <gerardo.medina103@alu.ulpgc.es>
2017-03-17 17:41:24 +0000

    allowed hosts

--- tollesforum/settings.py
+++ tollesforum/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$7xh+$myonbmd&wt3o_6mhv-b7cf($rz(%*gej^#2jncqoqn5-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [u'dastolleforum.herokuapp.com']
 
 
 # Application definition
