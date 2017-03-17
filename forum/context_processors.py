8159f02fc7e3a8affc15365c7cee36524248d588
Gerardo <gerardo.medina103@alu.ulpgc.es>
2017-03-17 18:21:07 +0000

    deleted avatar

--- forum/context_processors.py
+++
@@ -1,6 +0,0 @@
-from forum.models import UserProfile
-
-def profile_photo(request):
-    return {
-        'profile_photo': UserProfile.objects.get(user=request.user).photo.url
-    }
--- tollesforum/settings.py
+++ tollesforum/settings.py
@@ -71,7 +71,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'forum.context_processors.profile_photo',
             ],
         },
     },
